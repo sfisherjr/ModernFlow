e7c610491d262a8798b7d083d9e767c340c54b1c
Steven Fisher <phantomfury409@gmail.com>
2016-01-19 00:36:58 -0500

    Changed to one page design, added .gitignore

--- index.php
+++ index.php
@@ -3,71 +3,36 @@
 
 <head>
     <title>Vapor Venue</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-    <div class="header">
-        <div class="logo">Modern</div>
-        <div class="slogan">Catchy Slogan</div>
-    </div>
-
-    <div class="navbar">
-        <ul>
-            <li class="nav-first active"><a href="#">Home</a></li><!--
-         --><li><a href="#">Sweg</a></li><!--
-         --><li><a href="#">Fam</a></li><!--
-         --><li class="nav-last"><a href="#">Contact Us</a></li>
-        </ul>
-
-        <ul class="right-nav">
-            <li><a href="">Login</a></li>
+    <div class="nav-container">
+        <ul class="nav-list">
+            <li class="nav-item"><a id="nav_home" href="#">Home</a></li>
+            <li class="nav-item"><a id="nav_juice" href="#">E-Liquid</a></li>
+            <li class="nav-item"><a id="nav_mods" href="#">Mods</a></li>
+            <li class="nav-item"><a id="nav_contact" href="#">Find Us</a></li>
         </ul>
     </div>
 
-    <div class="right-content">
-        <h3>News</h3>
-        <p><strong>Quisque at felis ut</strong></p>
-        <p>Maecenas sagittis ornare est, sed maximus lacus tincidunt et. Sed mattis arcu ex, in dapibus magna ultrices quis. Duis eget elit quam.</p>
-
-        <p><strong>Quisque at felis ut</strong></p>
-        <p>Maecenas sagittis ornare est, sed maximus lacus tincidunt et. Sed mattis arcu ex, in dapibus magna ultrices quis. Duis eget elit quam.</p>
-
-        <p><strong>Quisque at felis ut</strong></p>
-        <p>Maecenas sagittis ornare est, sed maximus lacus tincidunt et. Sed mattis arcu ex, in dapibus magna ultrices quis. Duis eget elit quam.</p>
-
-        <p><strong>Quisque at felis ut</strong></p>
-        <p>Maecenas sagittis ornare est, sed maximus lacus tincidunt et. Sed mattis arcu ex, in dapibus magna ultrices quis. Duis eget elit quam.</p>
-
-        <h3>Stay in Touch</h3>
-        <p style="text-align: center;">
-            <img src="img/social/32x32 PNG/32-facebook.png" />
-            <img src="img/social/32x32 PNG/32-twitter.png" />
-            <img src="img/social/32x32 PNG/32-linkedin.png" />
-            <img src="img/social/32x32 PNG/32-yahoo.png" />
-        </p>
+    <div id="logo-page" class="alt-page-1">
+        <span class="mid-helper"></span>
+        <img src="img/logo.png" width="50%" />
     </div>
 
-    <div class="left-content">
-        <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <br>
-        <a class="button" href="">Read More</a>
+    <div id="juice-page" class="alt-page-2">
     </div>
 
-    <div class="left-content">
-        <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <br>
-        <a class="button" href="">Read More</a>
+    <div id="mod-page" class="alt-page-1">
     </div>
 
-    <div class="left-content">
-        <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <br>
-        <a class="button" href="">Read More</a>
+    <div id="final-page" class="alt-page-2">
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/clickscroll.js"></script>
 </body>
 
 </html>
---
+++ js/clickscroll.js
@@ -0,0 +1,35 @@
+$("#nav_home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#logo-page").offset().top
+    }, 2000);
+});
+
+$("#nav_juice").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#juice-page").offset().top
+    }, 2000);
+});
+
+$("#nav_mods").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#mod-page").offset().top
+    }, 2000);
+});
+
+$("#nav_asc").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#asc-page").offset().top
+    }, 2000);
+});
+
+$("#nav_social").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#social-page").offset().top
+    }, 2000);
+});
+
+$("#nav_contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#final-page").offset().top
+    }, 2000);
+});
